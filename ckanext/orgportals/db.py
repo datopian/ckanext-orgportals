28bfdd6883c00c824133cf2e25a3fff6aab33c13
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2016-11-21 14:17:41 +0100

    Create database tables for the portal

--- ckanext/orgportals/db.py
+++ ckanext/orgportals/db.py
@@ -1,36 +1,83 @@
 import uuid
+import datetime
 
 import sqlalchemy as sa
 
 import ckan.model as model
 
-portal_table = None
-Portal = None
+pages_table = None
+Pages = None
+
+menu_table = None
+Menu = None
 
 
-def make_uuid():
+def _make_uuid():
     return unicode(uuid.uuid4())
 
 
 def init():
+    _create_pages_table()
+    _create_menu_table()
 
-    class _Portal(model.DomainObject):
+
+def _create_pages_table():
+    class _Pages(model.DomainObject):
 
         @classmethod
         def get_foo(self):
             return 'foo'
 
-    global Portal
+    global Pages
 
-    Portal = _Portal
+    Pages = _Pages
 
-    portal_table = sa.Table('orgportals_page', model.meta.metadata,
-        sa.Column('id', sa.types.UnicodeText, primary_key=True, default=make_uuid),
-        sa.Column('foo', sa.types.UnicodeText),
-        sa.Column('foo', sa.types.UnicodeText),
+    pages_table = sa.Table('orgportal_pages', model.meta.metadata,
+        sa.Column('id', sa.types.UnicodeText, primary_key=True, default=_make_uuid),
+        sa.Column('organization_id', sa.types.UnicodeText, default=u''),
+        sa.Column('type', sa.types.UnicodeText, default=u''),
+        sa.Column('hero_image', sa.types.UnicodeText, default=u''),
+        sa.Column('title', sa.types.UnicodeText, default=u''),
+        sa.Column('text_box', sa.types.UnicodeText, default=u''),
+        sa.Column('content', sa.types.UnicodeText, default=u''),
+        sa.Column('email_address', sa.types.UnicodeText, default=u''),
+        sa.Column('themes', sa.types.String, default=u''),
+        sa.Column('datasets_per_page', sa.types.INT, default=u''),
+        sa.Column('survey_enabled', sa.types.Boolean, default=u''),
+        sa.Column('survey_text', sa.types.UnicodeText, default=u''),
+        sa.Column('survey_link', sa.types.UnicodeText, default=u''),
+        sa.Column('subdashboards', sa.types.String, default=u''),
+        sa.Column('created', sa.types.DateTime, default=datetime.datetime.utcnow),
+        sa.Column('modified', sa.types.DateTime, default=datetime.datetime.utcnow),
         extend_existing=True
     )
 
-    portal_table.create(checkfirst=True)
+    pages_table.create(checkfirst=True)
 
-    model.meta.mapper(Portal, portal_table)
+    model.meta.mapper(Pages, pages_table)
+
+
+def _create_menu_table():
+    class _Menu(model.DomainObject):
+
+        @classmethod
+        def get_foo(self):
+            return 'foo'
+
+    global Menu
+
+    Menu = _Menu
+
+    menu_table = sa.Table('orgportal_menu', model.meta.metadata,
+        sa.Column('id', sa.types.UnicodeText, primary_key=True, default=_make_uuid),
+        sa.Column('organization_id', sa.types.UnicodeText, default=u''),
+        sa.Column('title', sa.types.UnicodeText, default=u''),
+        sa.Column('order', sa.types.INT, default=u''),
+        sa.Column('created', sa.types.DateTime, default=datetime.datetime.utcnow),
+        sa.Column('modified', sa.types.DateTime, default=datetime.datetime.utcnow),
+        extend_existing=True
+    )
+
+    menu_table.create(checkfirst=True)
+
+    model.meta.mapper(Menu, menu_table)
