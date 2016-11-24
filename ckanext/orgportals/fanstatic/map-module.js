bfcfe3bde9aa4c008292e8f5ea9366d1a6ecd936
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2016-11-24 11:05:00 +0100

    Cleanup code

--- ckanext/orgportals/fanstatic/map-module.js
+++ ckanext/orgportals/fanstatic/map-module.js
@@ -4,10 +4,10 @@ ckan.module('orgportals_map', function ($, _) {
   return {
     initialize: function() {
       ckan.orgportals.dashboardmap.init(this.options.id,
-                                           this.options.organizationName,
-                                           this.options.mapurl,
-                                           this.options.color,
-                                           this.options.main_property);
+                                        this.options.organizationName,
+                                        this.options.mapurl,
+                                        this.options.color,
+                                        this.options.main_property);
     }
   }
 });
