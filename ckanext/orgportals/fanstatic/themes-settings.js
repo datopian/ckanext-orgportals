6c3fa56fa9b1734f50b74fc1855d04015ddd245f
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2016-11-28 20:23:41 +0100

    Improve adding/removing a dashboard

--- ckanext/orgportals/fanstatic/themes-settings.js
+++ ckanext/orgportals/fanstatic/themes-settings.js
@@ -1,20 +1,58 @@
 (function themesSettings() {
 
   var createDashboardBtn = $('#create-dashboard-btn');
-  var subdashboardFields = [
-    ''
-  ];
   var subdashboardFields = $('.themes-settings-subdashboard-fields');
   var themesProperties = $('.themes-properties');
+  var themeItems;
+  var totalItems;
 
+  // Add a theme/dashboard
   createDashboardBtn.on('click', function onCreateDashboardBtnClick() {
     var newFields = subdashboardFields.contents().clone();
 
+    themeItems = $('.themes-settings-subdashboard-fields__item');
+    totalItems = themeItems.length;
+
+    newFields.each(function(i, item) {
+      var item = $(item);
+      var themeTitle;
+      var themeGroup;
+      var themeEnabled;
+
+      if (item.hasClass('themes-settings-subdashboard-fields__item')) {
+        themeTitle = item.find('#theme_title');
+        themeGroup = item.find('#theme_group');
+        themeEnabled = item.find('#theme_enabled');
+
+        themeTitle.attr('id', 'theme_title_' + totalItems);
+        themeTitle.attr('name', 'theme_title_' + totalItems);
+
+        themeGroup.attr('id', 'theme_group_' + totalItems);
+        themeGroup.attr('name', 'theme_group_' + totalItems);
+
+        themeEnabled.attr('id', 'theme_enabled_' + totalItems);
+        themeEnabled.attr('name', 'theme_enabled_' + totalItems);
+      }
+    });
+
     themesProperties.append(newFields);
+
+    // Limit the total number of themes to 6
+    if (totalItems == 6) {
+      createDashboardBtn.css('display', 'none');
+    }
   });
 
+  // Remove a theme/dashboard
   themesProperties.on('click', 'a', function (e) {
     $(e.target).parent().remove();
+
+    themeItems = $('.themes-settings-subdashboard-fields__item');
+    totalItems = themeItems.length;
+
+    if (totalItems < 7) {
+      createDashboardBtn.css('display', 'initial');
+    }
   });
 
 })();
