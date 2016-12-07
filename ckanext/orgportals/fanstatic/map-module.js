ca4ade1b6dbfa9b8c23c3f6f0f4e1c8c798ee576
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2016-12-07 17:54:21 +0100

    Highlight country borders on the map

--- ckanext/orgportals/fanstatic/map-module.js
+++ ckanext/orgportals/fanstatic/map-module.js
@@ -4,10 +4,12 @@ ckan.module('orgportals_map', function ($, _) {
   return {
     initialize: function() {
       ckan.orgportals.dashboardmap.init(this.options.id,
-                                        this.options.organizationName,
+                                        this.options.organization_name,
                                         this.options.mapurl,
                                         this.options.color,
-                                        this.options.main_property);
+                                        this.options.main_property,
+                                        this.options.map_selector_name,
+                                        this.options.organization_entity_name);
     }
   }
 });
